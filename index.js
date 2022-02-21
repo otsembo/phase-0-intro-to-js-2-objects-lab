ef7edf18194573ba0143189fe4ff35edbcb9e399
otsembo <okumu.otsembo@gmail.com>
2022-02-21 15:13:20 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "Ian",
+    streetAddress: "Home"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let clone = {...employee};
+    clone[key] = value;
+    return clone;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let clone = {...employee};
+    delete clone[key];
+    return clone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
